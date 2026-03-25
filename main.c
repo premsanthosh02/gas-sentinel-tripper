1d160bde63f8b604b3eb8e76b9c1cadd00e2bb26
premsanthosh02 <2300069019eee@gmail.com>
2026-03-25 21:07:16 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Gas Sentinel Tripper System Initialized\n");
+    printf("Monitoring LPG gas levels...\n");
+    printf("If gas detected -> MCB will trip automatically\n");
+    return 0;
+}
